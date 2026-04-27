fcb7c616c48d9c2a1e8e6b76c3dba5e633495a4c
sla <goohanapelao@gmail.com>
2026-04-27 16:56:45 -0300

    teste
    a

---
+++ ProjectAspMvc/Controllers/feature-cidade.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectAspMvc.Controllers
+{
+    public class feature_cidade : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ProjectAspMvc/Controllers/feature-estado.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography;
+
+namespace ProjectAspMvc.Controllers
+{
+    public class feature_estado : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+            
+        }
+    }
+}
